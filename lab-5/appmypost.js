d71b5df40d62780f946bfe768f41c97d8cb160e8
Alex Vrsecky <vrsecky102@gmail.com>
2024-11-28 09:56:36 +1100

    Started 5.1

---
+++ lab-5/appmypost.js
@@ -0,0 +1,47 @@
+
+
+Vue.component('app-mypost', {
+    data: {},
+    template: ""
+})
+
+const app = Vue.createApp({})
+
+app.mount("#app")
+
+
+
+
+
+
+
+
+
+
+// Vue.component('app-mypost', // indicating the component tag
+//     {
+//       // defining data to be used in the component
+//       data: function(){
+//         return {
+//           statPosts: [],
+//           strStatus: ''
+//         }
+//       },
+//       // define the template for the component
+//       template: 
+//       // your code here
+//       `,
+//       // defining the methods for add and remove status messages
+//       methods: {
+//         add: function(status){
+//           // push status into statPosts array
+//         },
+//         remove: function(index){
+//           // delete status from statPosts using index
+//         }
+//       }
+//     });
+    
+//     new Vue({
+//       el: "#app"
+//     });
