37fb133ef5cc257ffa08274a676d582b25ec58c9
Alex Vrsecky <vrsecky102@gmail.com>
2024-11-27 22:17:21 +1100

    finished 4.1

--- lab-4/appgame.js
+++ lab-4/appgame.js
@@ -1,14 +1,42 @@
-import createApp from 'vue'
+const app = Vue.createApp({
+  data() {
+    const guess = null;
+    const randomInt = null;
+    const activeAttempt = false;
+    message = "";
+    return {
+      guess,
+      randomInt,
+      activeAttempt,
+      message
+    };
+  },
+  methods: {
+    checkGuess(e) {
+      if (this.activeAttempt === false) {
+        this.activeAttempt = true;
+        this.randomInt = this.generateRandomInt();
+      } else if (this.guess === null) return;
 
-const app = createApp({
-    data() {
-        
+      if (this.randomInt === this.guess) {
+        this.message = "You got it!"
+      } else if (this.randomInt > this.guess) {
+        this.message = "Guess higher"
+      } else if (this.randomInt < this.guess) {
+        this.message = "Guess lower"
+      };
     },
-    methods: {
-        plus() {
-            console.log('plus');
-        }
-    }
-})
+    giveUp() {
+        this.message = `The number was ${this.randomInt}`;
+    },
+    startOver() {
+        this.guess = null;
+        this.randomInt = null;
+    },
+    generateRandomInt() {
+      return Math.floor(Math.random() * 101);
+    },
+  },
+});
 
-app.mount("#app")
+app.mount("#app");
