446e5d2f7139f3511c717ee26e274fb212e946b7
Alex Vrsecky <vrsecky102@gmail.com>
2024-11-28 11:01:51 +1100

    finished 5.1

--- lab-5/appmypost.js
+++ lab-5/appmypost.js
@@ -1,47 +1,42 @@
+const app = Vue.createApp({});
 
+app.component("app-mypost", {
+  // defining data to be used in the component
+  data: function () {
+    return {
+      statPosts: [],
+      strStatus: "",
+    };
+  },
+// define the template for the component
+  template: `
+    <div class="container m-5">
+      <div>
+        <span>
+          <slot/> <input type="text" name="status" id="status" v-model="strStatus" />
+          <button type="button" class="mx-2 rounded" @click="add(strStatus)">Post</button>
+        </span>
+      </div>
+      <div class="mt-3">
+        <p v-for="(status, index) in statPosts" :key="index">
+        {{status}} <button @click="del(index)" class="rounded" type="button">Del</button>
+        </p>
+      </div>
+    </div>
+  `,
+// defining the methods for add and remove status messages
+  methods: {
+    add: function(status) {
+      // push status into statPosts array
+      this.statPosts.push(status);
+      this.strStatus = "";
+    },
+    del: function(index) {
+       // delete status from statPosts using index
+      this.statPosts.splice(index, 1)
+    }
+  }
+});
 
-Vue.component('app-mypost', {
-    data: {},
-    template: ""
-})
+app.mount("#app");
 
-const app = Vue.createApp({})
-
-app.mount("#app")
-
-
-
-
-
-
-
-
-
-
-// Vue.component('app-mypost', // indicating the component tag
-//     {
-//       // defining data to be used in the component
-//       data: function(){
-//         return {
-//           statPosts: [],
-//           strStatus: ''
-//         }
-//       },
-//       // define the template for the component
-//       template: 
-//       // your code here
-//       `,
-//       // defining the methods for add and remove status messages
-//       methods: {
-//         add: function(status){
-//           // push status into statPosts array
-//         },
-//         remove: function(index){
-//           // delete status from statPosts using index
-//         }
-//       }
-//     });
-    
-//     new Vue({
-//       el: "#app"
-//     });
