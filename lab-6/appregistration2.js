13883fb40fa23e9298372868ec64878dfb8b706a
Alex Vrsecky <vrsecky102@gmail.com>
2024-12-31 09:55:30 +1100

    second

--- lab-6/appregistration2.js
+++ lab-6/appregistration2.js
@@ -1,5 +1,25 @@
-const app = Vue.createApp();
+const app = Vue.createApp({
+  data() {
+    return {
+      fName: "",
+      lName: "",
+      uName: "",
+      password: "",
+      RePassword: "",
+      Email: "",
+      StreetAddress: "",
+      Suburb: "",
+      Postcode: "",
+      Mobile: "",
+      showTerms: false,
+      errors: {}
+    };
+  },
+  methods: {
+    validate: function () {
+        
+    }
+  },
+});
 
-
-
-app.mount('#app')
+app.mount("#app");
